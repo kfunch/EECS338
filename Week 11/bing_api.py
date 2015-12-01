294629e834d77b1119960718e580009c8e33fe79
kfunch <kristinfunch2016@u.northwestern.edu>
2015-11-30 21:00:24 -0600

    Create bing_api.py

---
+++ Week 11/bing_api.py
@@ -0,0 +1,34 @@
+import urllib
+import urllib2
+import json
+ 
+def main():
+    query = raw_input('Query: ')
+    bing_api(query)
+
+def bing_api(query):
+    keyBing = ''        # get Bing key from: https://datamarket.azure.com/account/keys
+    credentialBing = 'Basic ' + (':%s' % keyBing).encode('base64')[:-1]
+    query = query.replace('\"', '%22')
+    searchString = '%27' + query.replace(' ', '+') + '%27'
+    top = 10
+    offset = 0
+
+    url = 'https://api.datamarket.azure.com/Bing/Search/Web?' + \
+          'Query=%s&$top=%d&$skip=%d&$format=json' % (searchString, top, offset)
+
+    #print url
+    request = urllib2.Request(url)
+    request.add_header('Authorization', credentialBing)
+    requestOpener = urllib2.build_opener()
+    response = requestOpener.open(request) 
+
+    results = json.load(response)
+    #print results
+    #for x in results['d']['results']:
+    #   print x['Url']
+    #print "no urls?"
+    return results
+
+if __name__ == "__main__":
+    main()
